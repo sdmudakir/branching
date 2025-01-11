d054ccf7ea6fb5393ba48bd983824a9ff8ab330e
sdmudakir <sdmudakir@gmail.com>
2025-01-11 15:06:25 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import boto3
+
+client = boto3.client('ec2')
+response = client.run_instances(
+    ImageId='ami-0453ec754f44f9a4a',
+    InstanceType='t2.micro',
+    KeyName = 'devops',
+
+    MaxCount=1,
+    MinCount=1,
+)
